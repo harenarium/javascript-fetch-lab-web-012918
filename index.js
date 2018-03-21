ad37cb1d7113309df9a820c4a6000e4597226fa6
harenarium <22226614+harenarium@users.noreply.github.com>
2018-03-20 21:29:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
+const url = 'https://api.github.com'
+
 function getIssues() {
+  //2. GET /repos/:owner/:repo/issues
+  fetch(url + '/repos/harenarium/javascript-fetch-lab/issues', {
+    headers: {Authorization: `token ${getToken()}`}
+  }).then(resp => resp.json()).then(json => showIssues(json))
 }
 
+
+
 function showIssues(json) {
+  json.forEach(issue => $('#issues').append(issue))
 }
 
 function createIssue() {
+  //1. POST /repos/:owner/:repo/issues
+  const issueTitle = document.getElementById('title').value
+  const issueText = document.getElementById('body').value
+  fetch(url + '/repos/harenarium/javascript-fetch-lab/issues', {
+    method: 'post',
+    headers: {Authorization: `token ${getToken()}`},
+    body: JSON.stringify({title: issueTitle, body: issueText})
+  }).then(resp => getIssues())
 }
 
 function showResults(json) {
+  $('#results').append(json[0].name)
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+
+  fetch(url + '/repos/' + repo + '/forks', {
+    method: 'post',
+    headers: {Authorization: `token ${getToken()}`}
+  }).then(resp => resp.json()).then(json => showResults(json))
 }
 
 function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
   return ''
 }
